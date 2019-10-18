6341d04b335c1506a9e0b5569fa498ff3e932218
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-18 02:05:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,11 @@
-class Createshows < ActiveRecord::Migration[5.2]
+class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
+      #primary key of :id is created for us!
       t.string :name
-      t.integer :network
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
+      t.string :day
+      t.string :network
+      t.integer :rating
     end
   end
 end
