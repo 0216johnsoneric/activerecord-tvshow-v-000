a1d315cc4f7a391ba9f6cb5d04d01ba509af6fe7
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-18 02:04:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,7 +2,7 @@ class Createshows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
       t.string :name
-      t.integer :release_date
+      t.integer :network
       t.string :director
       t.string :lead
       t.boolean :in_theaters
